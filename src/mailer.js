1c0a508eb69bdcd466bf2bdf4a3c01c39c293530
Vikas Yadav <vikasyadav14nov@gmail.com>
2024-07-26 13:51:43 +0530

    nodemailer

---
+++ src/mailer.js
@@ -0,0 +1,22 @@
+const mailOptions = {
+    from: 'vikas14nov2001@gmail.com',
+    to: 'pravinpatekar1999@gmail.com',
+    subject: 'Your One Time Password',
+    html: `<p>Enter <b>${otp}</b> in the app to reset Password</p>
+    <p>This code <b>expires in 1 hour</b></p>`,
+};
+transporter.sendMail(mailOptions, (error, done) => {
+    if (error) return res.status(500).send(error.message);
+    else return done;
+});
+
+const Nodemailer = require('nodemailer');
+const transporter = Nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+    },
+});
