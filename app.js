38c60c34747c4dccb256d789172eeeebb4f18d96
Vikas Yadav <vikasyadav14nov@gmail.com>
2024-07-26 15:55:43 +0530

    port

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const http = require('http').createServer(app);
 const db = require('./config/dbConnection')
 const bodyParser = require('body-parser');
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json())
 
 app.use('/api', require('./src/routes'));
 
-http.listen(PORT,(err)=>{
-    err ? 
-    console.log(`connection failed at port: ${PORT}`)
-    :
-    console.log(`connction successful port: ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+http.listen(PORT, (err) => {
+    console.log(PORT);
+    err ?
+        console.log(`connection failed at port: ${PORT}`)
+        :
+        console.log(`connction successful port: ${PORT}`);
+    console.log(PORT);
 })
